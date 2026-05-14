1501feb9feffedcbf0ac7a6c4713dd7f454eb9be
MStrike <131683655+JoeNutss@users.noreply.github.com>
2026-05-14 13:04:47 +0200

    electricity p2

--- Assets/Scripts/PlugTest.cs
+++ Assets/Scripts/PlugTest.cs
@@ -5,20 +5,54 @@ public class PlugTest : MonoBehaviour
     [SerializeField]
     float timer = 1f;
 
+    [SerializeField]
+    GameObject electricityVFX;
+
+    [SerializeField]
+    Material electricityCheck;
+
+
+    private void Start()
+    {
+       electricityVFX.SetActive(false);
+
+       //var electricityCheckMat = electricityCheck.GetComponent<Renderer>();
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Plug"))
         {
-            Debug.Log("Enter");
+            electricityVFX.SetActive(true);
 
             Invoke(nameof(TimerOut), timer);
+        }
+
+        if (other.gameObject.CompareTag("ElectricityCheck"))
+        {
+            if (electricityVFX.activeInHierarchy)
+            {
+                electricityCheck.SetColor("_BaseColor", Color.green);
+            }
+            else
+            {
+                electricityCheck.SetColor("_BaseColor", Color.red);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("ElectricityCheck"))
+        {
+            electricityCheck.SetColor("_BaseColor", Color.white);
         }
     }
 
 
     void TimerOut()
     {
-        Debug.Log("Time Out");
+        electricityVFX.SetActive(false);
     }
 }
